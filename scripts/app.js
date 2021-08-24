6baf9e6ed936ac5654014b3a009c2fd627b6ac88
AbabacarDieneNdiaye <ababacardienendiaye.dev@gmail.com>
2021-08-24 15:06:02 +0000

    responsive done

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,17 @@
 
-    function imgSlider(anything) {
-        document.querySelector('.starbucks').src = anything;
-    }
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
 
-    function changeCircleColor(color) {
-        const circle = document.querySelector('.circle');
-        circle.style.background = color;
-    }
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function toggleMenu() {
+    var menuToggle  = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+}
